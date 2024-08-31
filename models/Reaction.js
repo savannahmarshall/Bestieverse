90f67a18131640c91c9bc873a6a4a8de72866826
savannahmarshall <savvymarshall@gmail.com>
2024-08-30 19:09:44 -0700

    added schema for reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, Types } = require('mongoose');
+
+// Schema for Reaction 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(), 
+    },
+  },
+  {
+    toJSON: {
+      getters: true, 
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
